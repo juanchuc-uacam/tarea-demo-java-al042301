f5bbe1b05c9796c7292ed2b2e20b95f8939ced7c
al042301 <al042301@uacam.mx>
2024-06-16 14:47:45 -0600

    FIX: Segunda correción

--- src/main/java/com/example/project/HelloWorld.java
+++ src/main/java/com/example/project/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.project;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.print("El test marcará error\n");
+    System.out.print("Hello world!\n");
   }
 }
